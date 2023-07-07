9037584d5576d718473e73dd806a55eb873cf80e
Justin Tadlock <justintadlock@gmail.com>
2023-07-07 08:36:22 -0500

    Reverts custom template descriptions
    This seems to break the post/page editor when saving the template. Also, updates some of the template titles.

--- src/Templates.php
+++ src/Templates.php
@@ -40,16 +40,16 @@ class Templates implements Bootable
 	{
 		// Registers custom template titles for the core templates.
 		$template_titles = [
-			'404'        => _x( '404',              'Template name', 'x3p0-ideas' ),
+			'404'        => _x( 'Error 404',        'Template name', 'x3p0-ideas' ),
 			'archive'    => _x( 'Archive',          'Template name', 'x3p0-ideas' ),
 			'attachment' => _x( 'Media',            'Template name', 'x3p0-ideas' ),
 			'author'     => _x( 'Author Archive',   'Template name', 'x3p0-ideas' ),
 			'category'   => _x( 'Category Archive', 'Template name', 'x3p0-ideas' ),
 			'date'       => _x( 'Date Archive',     'Template name', 'x3p0-ideas' ),
 			'home'       => _x( 'Blog',             'Template name', 'x3p0-ideas' ),
-			'page'       => _x( 'Page',             'Template name', 'x3p0-ideas' ),
-			'single'     => _x( 'Single',           'Template name', 'x3p0-ideas' ),
-			'singular'   => _x( 'Singular',         'Template name', 'x3p0-ideas' ),
+			'page'       => _x( 'Single Page',      'Template name', 'x3p0-ideas' ),
+			'single'     => _x( 'Single Post',      'Template name', 'x3p0-ideas' ),
+			'singular'   => _x( 'Single Entry',     'Template name', 'x3p0-ideas' ),
 			'tag'        => _x( 'Tag Archive',      'Template name', 'x3p0-ideas' ),
 			'taxonomy'   => _x( 'Term Archive',     'Template name', 'x3p0-ideas' ),
 		];
@@ -60,34 +60,6 @@ class Templates implements Bootable
 			}
 		}
 
-		// Core doesn't yet support descriptions for custom template
-		// types, so we must register them via PHP. The downside is that
-		// we must also manually register the template title both here
-		// and in `theme.json`, so we must keep these in sync.
-		//
-		// @link https://github.com/WordPress/gutenberg/issues/44097
-		$custom_templates = [
-			'template-canvas' => [
-				'title'       => _x( 'Custom: Canvas', 'Template name', 'x3p0-ideas' ),
-				'description' => __( 'An open canvas template for single posts and pages, showing only the site header, footer, and content by default.', 'x3p0-ideas' )
-			],
-			'template-post-faded-cover' => [
-				'title'       => _x( 'Post: Faded Cover', 'Template name', 'x3p0-ideas' ),
-				'description' => __( 'Single post template that uses the featured image as the site header background with an overlay that fades into the background color.', 'x3p0-ideas' )
-			],
-			'template-post-featured' => [
-				'title'       => _x( 'Post: Featured', 'Template name', 'x3p0-ideas' ),
-				'description' => __( 'Single post template that puts the featured image below the header with the post title and metadata centered over it.', 'x3p0-ideas' )
-			],
-			'template-post-featured-cover' => [
-				'title'       => _x( 'Post: Featured Cover', 'Template name', 'x3p0-ideas' ),
-				'description' => __( 'Single post template that uses the featured image as the site header background with a dark overlay and light text.', 'x3p0-ideas' )
-			],
-		];
-
-		return [
-			...$default_template_types,
-			...$custom_templates
-		];
+		return $default_template_types;
 	}
 }
