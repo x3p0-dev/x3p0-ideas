fcf67e7bdf5d617620f028bcf01d8b4ee2abea11
Justin Tadlock <justintadlock@gmail.com>
2023-07-06 23:37:25 -0500

    Add custom template descriptions
    Core doesn't allow custom template descriptions via `theme.json`, so these must be added via PHP.
    
    See: https://github.com/WordPress/gutenberg/issues/44097

--- src/Templates.php
+++ src/Templates.php
@@ -29,15 +29,16 @@ class Templates implements Bootable
 	}
 
 	/**
-	 * Filter the core template part areas to add custom areas needed for
-	 * the theme.
+	 * Filter the core template types.
 	 *
 	 * @hook  default_template_types
 	 * @since 1.0.0
 	 * @link  https://developer.wordpress.org/reference/hooks/default_template_types/
+	 * @todo  Register missing core templates.
 	 */
 	public function defaultTemplateTypes( array $default_template_types ): array
 	{
+		// Registers custom template titles for the core templates.
 		$template_titles = [
 			'404'        => _x( '404',              'Template name', 'x3p0-ideas' ),
 			'archive'    => _x( 'Archive',          'Template name', 'x3p0-ideas' ),
@@ -59,6 +60,34 @@ class Templates implements Bootable
 			}
 		}
 
-		return $default_template_types;
+		// Core doesn't yet support descriptions for custom template
+		// types, so we must register them via PHP. The downside is that
+		// we must also manually register the template title both here
+		// and in `theme.json`, so we must keep these in sync.
+		//
+		// @link https://github.com/WordPress/gutenberg/issues/44097
+		$custom_templates = [
+			'template-canvas' => [
+				'title'       => _x( 'Custom: Canvas', 'Template name', 'x3p0-ideas' ),
+				'description' => __( 'An open canvas template for single posts and pages, showing only the site header, footer, and content by default.', 'x3p0-ideas' )
+			],
+			'template-post-faded-cover' => [
+				'title'       => _x( 'Post: Faded Cover', 'Template name', 'x3p0-ideas' ),
+				'description' => __( 'Single post template that uses the featured image as the site header background with an overlay that fades into the background color.', 'x3p0-ideas' )
+			],
+			'template-post-featured' => [
+				'title'       => _x( 'Post: Featured', 'Template name', 'x3p0-ideas' ),
+				'description' => __( 'Single post template that puts the featured image below the header with the post title and metadata centered over it.', 'x3p0-ideas' )
+			],
+			'template-post-featured-cover' => [
+				'title'       => _x( 'Post: Featured Cover', 'Template name', 'x3p0-ideas' ),
+				'description' => __( 'Single post template that uses the featured image as the site header background with a dark overlay and light text.', 'x3p0-ideas' )
+			],
+		];
+
+		return [
+			...$default_template_types,
+			...$custom_templates
+		];
 	}
 }
