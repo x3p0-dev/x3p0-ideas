311b195d1f3020119adde59f0cda6b6d779d345b
Justin Tadlock <justintadlock@gmail.com>
2023-12-18 12:05:26 -0600

    Add a FAQs pattern.

---
+++ patterns/faqs.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Title: FAQs
+ * Slug: x3p0-ideas/faqs
+ * Description: Outputs a list of configurable FAQ items.
+ * Categories: text
+ * Keywords: faq, accordion, toggle, questions, answers
+ * Viewport Width: 640
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:details -->
+	<details class="wp-block-details">
+		<summary><?php esc_html_e( 'Question 1?', 'themeslug' ); ?></summary>
+		<!-- wp:paragraph {"placeholder":"<?php esc_attr_e( 'Add an answer to the question.', 'themeslug' ); ?>"} -->
+		<p></p>
+		<!-- /wp:paragraph -->
+	</details>
+	<!-- /wp:details -->
+
+	<!-- wp:details -->
+	<details class="wp-block-details">
+		<summary><?php esc_html_e( 'Question 2?', 'themeslug' ); ?></summary>
+		<!-- wp:paragraph {"placeholder":"<?php esc_attr_e( 'Add an answer to the question.', 'themeslug' ); ?>"} -->
+		<p></p>
+		<!-- /wp:paragraph -->
+	</details>
+	<!-- /wp:details -->
+
+	<!-- wp:details -->
+	<details class="wp-block-details">
+		<summary><?php esc_html_e( 'Question 3?', 'themeslug' ); ?></summary>
+		<!-- wp:paragraph {"placeholder":"<?php esc_attr_e( 'Add an answer to the question.', 'themeslug' ); ?>"} -->
+		<p></p>
+		<!-- /wp:paragraph -->
+	</details>
+	<!-- /wp:details -->
+
+	<!-- wp:details -->
+	<details class="wp-block-details">
+		<summary><?php esc_html_e( 'Question 4?', 'themeslug' ); ?></summary>
+		<!-- wp:paragraph {"placeholder":"<?php esc_attr_e( 'Add an answer to the question.', 'themeslug' ); ?>"} -->
+		<p></p>
+		<!-- /wp:paragraph -->
+	</details>
+	<!-- /wp:details -->
+</div>
+<!-- /wp:group -->
