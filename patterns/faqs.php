0cd9682e145de9fcb80ed90533f450a6b9f917a4
Justin Tadlock <justintadlock@gmail.com>
2023-12-28 11:21:23 -0600

    Use loop in FAQs pattern.

--- patterns/faqs.php
+++ patterns/faqs.php
@@ -10,40 +10,21 @@
 ?>
 <!-- wp:group {"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
-	<!-- wp:details -->
-	<details class="wp-block-details">
-		<summary><?php esc_html_e( 'Question 1?', 'x3p0-ideas' ); ?></summary>
-		<!-- wp:paragraph {"placeholder":"<?php esc_attr_e( 'Add an answer to the question.', 'x3p0-ideas' ); ?>"} -->
-		<p></p>
-		<!-- /wp:paragraph -->
-	</details>
-	<!-- /wp:details -->
 
-	<!-- wp:details -->
-	<details class="wp-block-details">
-		<summary><?php esc_html_e( 'Question 2?', 'x3p0-ideas' ); ?></summary>
-		<!-- wp:paragraph {"placeholder":"<?php esc_attr_e( 'Add an answer to the question.', 'x3p0-ideas' ); ?>"} -->
-		<p></p>
-		<!-- /wp:paragraph -->
-	</details>
-	<!-- /wp:details -->
+	<?php foreach ( range( 1, 4 ) as $number ) : ?>
+		<!-- wp:details -->
+		<details class="wp-block-details">
+			<summary>
+				<?php printf( __( 'Question %d?', 'x3p0-ideas'), $number ) ?>
+			</summary>
+			<!-- wp:paragraph {
+				"placeholder":"<?php esc_attr_e( 'Add an answer to the question.', 'x3p0-ideas' ) ?>"
+			} -->
+			<p></p>
+			<!-- /wp:paragraph -->
+		</details>
+		<!-- /wp:details -->
+	<?php endforeach ?>
 
-	<!-- wp:details -->
-	<details class="wp-block-details">
-		<summary><?php esc_html_e( 'Question 3?', 'x3p0-ideas' ); ?></summary>
-		<!-- wp:paragraph {"placeholder":"<?php esc_attr_e( 'Add an answer to the question.', 'x3p0-ideas' ); ?>"} -->
-		<p></p>
-		<!-- /wp:paragraph -->
-	</details>
-	<!-- /wp:details -->
-
-	<!-- wp:details -->
-	<details class="wp-block-details">
-		<summary><?php esc_html_e( 'Question 4?', 'x3p0-ideas' ); ?></summary>
-		<!-- wp:paragraph {"placeholder":"<?php esc_attr_e( 'Add an answer to the question.', 'x3p0-ideas' ); ?>"} -->
-		<p></p>
-		<!-- /wp:paragraph -->
-	</details>
-	<!-- /wp:details -->
 </div>
 <!-- /wp:group -->
