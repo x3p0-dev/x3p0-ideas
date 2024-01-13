9f49fbcaedce20aa77356cca75d1f902bbac09e3
Justin Tadlock <justintadlock@gmail.com>
2024-01-13 12:23:32 -0600

    Add the grid variation for the Group block.
    This has existed in Gutenberg for ages, but it still hasn't shipped with WP. Might as well ship it with the theme.

--- public/js/editor.asset.php
+++ public/js/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text', 'wp-token-list'), 'version' => 'c1982e19829d9cacdfbc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text', 'wp-token-list'), 'version' => '1215dabaeabaa1643d6e');
--- resources/js/block-variations/index.js
+++ resources/js/block-variations/index.js
@@ -8,7 +8,12 @@
 
 // WordPress dependencies.
 import { __ } from '@wordpress/i18n';
-import { registerBlockVariation } from '@wordpress/blocks';
+import { grid } from '@wordpress/icons';
+import {
+	getBlockVariations,
+	registerBlockVariation
+} from '@wordpress/blocks';
+import domReady from '@wordpress/dom-ready';
 
 // Registers the theme spacer as the default so that---with any luck---users
 // will choose the theme spacing sizes over custom sizes. Note that there is
@@ -26,3 +31,24 @@ registerBlockVariation( 'core/spacer', {
 		blockAttributes.height && blockAttributes.height.includes( 'var:preset|spacing|' )
 } );
 
+// `getBlockVariations()` returns `undefined` unless we wait until the DOM is
+// ready. We need this to conditionally register variations.
+domReady( () => {
+	const variations = getBlockVariations( 'core/group' );
+
+	// Registers a grid variation for the Group block. This exists in the
+	// Gutenberg plugin, and we're just plugging it in here until it gets
+	// ported to WordPress at some point.
+	if ( ! variations.some( variation => 'group-grid' === variation.name ) ) {
+		registerBlockVariation( 'core/group', {
+			name: 'group-grid',
+			title: __( 'Grid', 'x3p0-ideas' ),
+			icon: grid,
+			description: __( 'Arrange blocks in a grid.', 'x3p0-ideas' ),
+			attributes: { layout: { type: 'grid' } },
+			scope: [ 'block', 'inserter', 'transform' ],
+			isActive: ( blockAttributes ) =>
+				blockAttributes.layout?.type === 'grid',
+		} );
+	}
+} );
