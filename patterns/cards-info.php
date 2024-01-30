98d4503284fb787000e4ebc5f7e8f28ca992be55
Justin Tadlock <justintadlock@gmail.com>
2024-01-30 01:04:47 -0600

    Add info cards pattern.

---
+++ patterns/cards-info.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * Title: Info Cards
+ * Slug: x3p0-ideas/cards-info
+ * Categories: x3p0-grid
+ * Keywords: card, grid
+ * Viewport Width: 1376
+ */
+
+declare(strict_types=1);
+
+# Prevent direct access.
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {
+	"tagName":"section",
+	"metadata":{"name":"<?= esc_attr__('Cards Container', 'x3p0-ideas') ?>"},
+	"align":"full",
+	"style":{
+		"spacing":{
+			"padding":{
+				"right":"var:preset|spacing|plus-3",
+				"left":"var:preset|spacing|plus-3",
+				"top":"var:preset|spacing|plus-4",
+				"bottom":"var:preset|spacing|plus-4"
+			}
+		}
+	},
+	"gradient":"90-deg-neutral-50-transparent",
+	"layout":{"type":"constrained"}
+} -->
+<section class="wp-block-group alignfull has-90-deg-neutral-50-transparent-gradient-background has-background" style="padding-top:var(--wp--preset--spacing--plus-4);padding-right:var(--wp--preset--spacing--plus-3);padding-bottom:var(--wp--preset--spacing--plus-4);padding-left:var(--wp--preset--spacing--plus-3)">
+
+	<!-- wp:group {
+		"tagName":"header",
+		"metadata":{"name":"<?= esc_attr__('Cards Header', 'x3p0-ideas') ?>"},
+		"style":{"spacing":{"blockGap":"var:preset|spacing|base"}},
+		"layout":{"type":"constrained"}
+	} -->
+	<header class="wp-block-group">
+
+		<!-- wp:heading {"textAlign":"center"} -->
+		<h2 class="wp-block-heading has-text-align-center"><?= esc_html__('Placeholder Heading', 'x3p0-ideas') ?></h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph {"align":"center"} -->
+		<p class="has-text-align-center"><?= esc_html__('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam interdum turpis quis metus aliquam, id pharetra arcu dignissim. Sed in sollicitudin mi.', 'x3p0-ideas') ?></p>
+		<!-- /wp:paragraph -->
+
+	</header>
+	<!-- /wp:group -->
+
+	<!-- wp:group {
+		"metadata":{"name":"<?= esc_attr__('Grid', 'x3p0-ideas') ?>"},
+		"align":"wide",
+		"layout":{"type":"grid","minimumColumnWidth":"16rem"}
+	} -->
+	<div class="wp-block-group alignwide">
+
+		<?php foreach ($icons as $icon) : ?>
+
+			<!-- wp:group {
+				"style":{
+					"spacing":{
+						"blockGap":"var:preset|spacing|minus-3",
+						"padding":{
+							"top":"var:preset|spacing|base",
+							"bottom":"var:preset|spacing|base",
+							"left":"var:preset|spacing|base",
+							"right":"var:preset|spacing|base"
+						}
+					},
+					"border":{"radius":"4px"}
+				},
+				"className":"is-style-card",
+				"layout":{"type":"default"},
+				"fontSize":"xs"
+			} -->
+			<div class="wp-block-group is-style-card has-xs-font-size" style="border-radius:4px;padding-top:var(--wp--preset--spacing--base);padding-right:var(--wp--preset--spacing--base);padding-bottom:var(--wp--preset--spacing--base);padding-left:var(--wp--preset--spacing--base)">
+
+				<!-- wp:heading {"level":3,"fontSize":"md"} -->
+				<h3 class="wp-block-heading has-md-font-size" id="browse-the-resources"><a href="#"><?= esc_html__('Placeholder Text', 'x3p0-ideas') ?></a></h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph -->
+				<p><?= esc_html__('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in sollicitudin mi.', 'x3p0-ideas') ?></p>
+				<!-- /wp:paragraph -->
+
+			</div>
+			<!-- /wp:group -->
+
+		<?php endforeach ?>
+
+	</div>
+	<!-- /wp:group -->
+
+</section>
+<!-- /wp:group -->
