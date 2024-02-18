70fa5247f886978100ec41b2c17f986d1d7ed0e2
Justin Tadlock <justintadlock@gmail.com>
2024-02-18 01:11:58 -0600

    Split attachment image to pattern.

---
+++ patterns/hidden-attachment-media-image.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Title: Attachment Media: Image
+ * Slug: x3p0-ideas/attachment-media-image
+ * Inserter: no
+ */
+
+declare(strict_types=1);
+
+# Prevent direct access.
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull">
+
+	<!-- wp:image {
+		"align":"wide",
+		"sizeSlug":"x3p0-16x9-lg",
+		"linkDestination":"none",
+		"metadata":{
+			"bindings":{
+				"url":{
+					"source":"x3p0/media",
+					"args":{
+						"type":"image",
+						"size":"x3p0-16x9-lg"
+					}
+				},
+				"alt":{
+					"source":"x3p0/media"
+				}
+			}
+		}
+	} -->
+	<figure class="wp-block-image alignwide size-x3p0-16x9-lg">
+		<img src="" alt="" />
+	</figure>
+	<!-- /wp:image -->
+
+</div>
+<!-- /wp:group -->
