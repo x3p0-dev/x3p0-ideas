4560f31f97acc9235f2fa5d083d9e8abb45a43b1
Justin Tadlock <justintadlock@gmail.com>
2024-06-29 19:01:35 -0500

    Split individual comments into a pattern.

--- patterns/comments-default.php
+++ patterns/comments-default.php
@@ -3,7 +3,6 @@
 /**
  * Title: Comments: Default
  * Slug: x3p0-ideas/comments-default
- * Description:
  * Keywords: comments, discussion
  * Block Types: core/comments
  * Viewport Width: 1376
@@ -28,118 +27,7 @@ defined('ABSPATH') || exit;
 
 		<!-- wp:comment-template -->
 
-			<!-- wp:group {
-				"tagName":"article",
-				"metadata":{"name":"<?= esc_attr__('Comment', 'x3p0-ideas') ?>"},
-				"layout":{"type":"default"}
-			} -->
-			<article class="wp-block-group">
-
-				<!-- wp:group {
-					"tagName":"header",
-					"metadata":{
-						"name":"<?= esc_attr__('Comment Header', 'x3p0-ideas') ?>"
-					},
-					"style":{
-						"spacing":{
-							"blockGap":"var:preset|spacing|base"
-						}
-					},
-					"layout":{
-						"type":"flex",
-						"flexWrap":"wrap"
-					},
-					"className":"is-style-comment-meta"
-				} -->
-				<header class="wp-block-group is-style-comment-meta">
-
-					<?php if (get_option('thread_comments')) : ?>
-
-						<!-- wp:paragraph {
-							"placeholder":"<?= esc_attr__('In reply to Comment Author', 'x3p0-ideas') ?>",
-							"metadata":{
-								"bindings":{
-									"content":{
-										"source":"x3p0/theme",
-										"args":{
-											"key":"commentParentLink"
-										}
-									}
-								},
-								"@ifAttribute":"content"
-							},
-							"style":{
-								"layout":{
-									"selfStretch":"fixed",
-									"flexSize":"100%"
-								}
-							},
-							"className":"comment-parent-link"
-						} -->
-						<p class="comment-parent-link"></p>
-						<!-- /wp:paragraph -->
-
-					<?php endif ?>
-
-					<!-- wp:avatar {
-						"size":56,
-						"style":{
-							"layout":{
-								"selfStretch":"fit",
-								"flexSize":null
-							}
-						}
-					} /-->
-
-					<!-- wp:group {
-						"metadata":{"name":"<?= esc_attr__('Comment Meta', 'x3p0-ideas') ?>"},
-						"layout":{"type":"default"}
-					} -->
-					<div class="wp-block-group">
-
-						<!-- wp:comment-author-name /-->
-
-						<!-- wp:group {
-							"style":{
-								"spacing":{
-									"margin":{"top":"0px","bottom":"0px"},
-									"blockGap":"var:preset|spacing|base"
-								}
-							},
-							"layout":{"type":"flex"}
-						} -->
-						<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px">
-							<!-- wp:comment-date /-->
-							<!-- wp:comment-edit-link /-->
-						</div>
-						<!-- /wp:group -->
-
-					</div>
-					<!-- /wp:group -->
-
-				</header>
-				<!-- /wp:group -->
-
-				<!-- wp:comment-content /-->
-
-				<!-- wp:group {
-					"tagName":"footer",
-					"metadata":{"name":"<?= esc_attr__('Comment Footer', 'x3p0-ideas') ?>"},
-					"style":{"spacing":{"blockGap":"var:preset|spacing|minus-2"}},
-					"layout":{
-						"type":"flex",
-						"flexWrap":"nowrap",
-						"justifyContent":"right"
-					},
-					"fontSize":"sm"
-				} -->
-				<footer class="wp-block-group has-sm-font-size">
-					<!-- wp:comment-reply-link /-->
-				</footer>
-				<!-- /wp:group -->
-
-			</article>
-			<!-- /wp:group -->
+			<!-- wp:pattern {"slug":"x3p0-ideas/comment"} /-->
 
 		<!-- /wp:comment-template -->
 
@@ -147,7 +35,10 @@ defined('ABSPATH') || exit;
 
 			<!-- wp:comments-pagination {
 				"paginationArrow":"arrow",
-				"layout":{"type":"flex","justifyContent":"space-between"}
+				"layout":{
+					"type":"flex",
+					"justifyContent":"space-between"
+				}
 			} -->
 				<!-- wp:comments-pagination-previous /-->
 				<!-- wp:comments-pagination-numbers /-->
---
+++ patterns/hidden-comment.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * Title: Comment
+ * Slug: x3p0-ideas/comment
+ * Inserter: no
+ */
+
+declare(strict_types=1);
+
+# Prevent direct access.
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {
+	"tagName":"article",
+	"metadata":{"name":"<?= esc_attr__('Comment', 'x3p0-ideas') ?>"},
+	"layout":{"type":"default"}
+} -->
+<article class="wp-block-group">
+
+	<!-- wp:group {
+		"tagName":"header",
+		"metadata":{
+			"name":"<?= esc_attr__('Comment Header', 'x3p0-ideas') ?>"
+		},
+		"style":{
+			"spacing":{
+				"blockGap":"var:preset|spacing|base"
+			}
+		},
+		"layout":{
+			"type":"flex",
+			"flexWrap":"wrap"
+		},
+		"className":"is-style-comment-meta"
+	} -->
+	<header class="wp-block-group is-style-comment-meta">
+
+		<?php if (get_option('thread_comments')) : ?>
+
+			<!-- wp:paragraph {
+				"placeholder":"<?= esc_attr__('In reply to Comment Author', 'x3p0-ideas') ?>",
+				"metadata":{
+					"bindings":{
+						"content":{
+							"source":"x3p0/theme",
+							"args":{
+								"key":"commentParentLink"
+							}
+						}
+					},
+					"@ifAttribute":"content"
+				},
+				"style":{
+					"layout":{
+						"selfStretch":"fixed",
+						"flexSize":"100%"
+					}
+				},
+				"className":"comment-parent-link"
+			} -->
+			<p class="comment-parent-link"></p>
+			<!-- /wp:paragraph -->
+
+		<?php endif ?>
+
+		<!-- wp:avatar {
+			"size":56,
+			"style":{
+				"layout":{
+					"selfStretch":"fit",
+					"flexSize":null
+				}
+			}
+		} /-->
+
+		<!-- wp:group {
+			"metadata":{"name":"<?= esc_attr__('Comment Meta', 'x3p0-ideas') ?>"},
+			"layout":{"type":"default"}
+		} -->
+		<div class="wp-block-group">
+
+			<!-- wp:comment-author-name /-->
+
+			<!-- wp:group {
+				"style":{
+					"spacing":{
+						"margin":{
+							"top":"0px",
+							"bottom":"0px"
+						},
+						"blockGap":"var:preset|spacing|base"
+					}
+				},
+				"layout":{"type":"flex"}
+			} -->
+			<div class="wp-block-group" style="margin-top:0px;margin-bottom:0px">
+				<!-- wp:comment-date /-->
+				<!-- wp:comment-edit-link /-->
+			</div>
+			<!-- /wp:group -->
+
+		</div>
+		<!-- /wp:group -->
+
+	</header>
+	<!-- /wp:group -->
+
+	<!-- wp:comment-content /-->
+
+	<!-- wp:group {
+		"tagName":"footer",
+		"metadata":{"name":"<?= esc_attr__('Comment Footer', 'x3p0-ideas') ?>"},
+		"style":{
+			"spacing":{
+				"blockGap":"var:preset|spacing|minus-2"
+			}
+		},
+		"layout":{
+			"type":"flex",
+			"flexWrap":"nowrap",
+			"justifyContent":"right"
+		}
+	} -->
+	<footer class="wp-block-group">
+		<!-- wp:comment-reply-link /-->
+	</footer>
+	<!-- /wp:group -->
+
+</article>
+<!-- /wp:group -->
+
