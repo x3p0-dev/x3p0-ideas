86c2169a9722ee24347be07dbb645dd18e13361d
Justin Tadlock <justintadlock@gmail.com>
2024-09-05 23:04:34 -0500

    Add a Comments: Plain pattern.

---
+++ patterns/comments-plain.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Title: Comments: Plain
+ * Slug: x3p0-ideas/comments-plain
+ * Keywords: comments, discussion
+ * Block Types: core/comments, core/template-part/comments
+ * Viewport Width: 1376
+ */
+
+declare(strict_types=1);
+
+# Prevent direct access.
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:comments {
+	"style":{
+		"spacing":{
+			"padding":{
+				"top":"var:preset|spacing|plus-3",
+				"bottom":"var:preset|spacing|plus-3"
+			}
+		}
+	},
+	"layout":{"type":"constrained"}
+} -->
+<div class="wp-block-comments" style="padding-top:var(--wp--preset--spacing--plus-3);padding-bottom:var(--wp--preset--spacing--plus-3)">
+
+	<!-- wp:group {
+		"metadata":{
+			"name":"<?= esc_attr__('Comments Container', 'x3p0-ideas') ?>",
+			"@if":"get_comments_number"
+		},
+		"layout":{"type":"constrained"}
+	} -->
+	<div class="wp-block-group">
+
+		<!-- wp:comments-title {
+			"showPostTitle":false,
+			"fontSize":"xl"
+		} /-->
+
+		<!-- wp:comment-template -->
+			<!-- wp:pattern {"slug":"x3p0-ideas/comment"} /-->
+		<!-- /wp:comment-template -->
+
+	</div>
+	<!-- /wp:group -->
+
+	<?php if (get_option('page_comments')) : ?>
+
+		<!-- wp:comments-pagination {
+			"paginationArrow":"arrow",
+			"layout":{
+				"type":"flex",
+				"justifyContent":"space-between"
+			}
+		} -->
+			<!-- wp:comments-pagination-previous /-->
+			<!-- wp:comments-pagination-numbers /-->
+			<!-- wp:comments-pagination-next /-->
+		<!-- /wp:comments-pagination -->
+
+	<?php endif ?>
+
+	<!-- wp:post-comments-form {
+		"className":"has-icons"
+	} /-->
+
+</div>
+<!-- /wp:comments -->
