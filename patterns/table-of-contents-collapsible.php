d234d0b83fd8f887535627338166ea5397906684
Justin Tadlock <justintadlock@gmail.com>
2025-07-04 20:06:10 -0500

    Add a collapsible table of contents pattern.

---
+++ patterns/table-of-contents-collapsible.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Title: Table of Contents - Collapsible
+ * Slug: x3p0-ideas/table-of-contents-collapsible
+ * Description: Displays a post's table of contents within a Details block so that it can be opened and closed.
+ * Categories: text
+ * Keywords: table, contents, list
+ * Block Types: core/table-of-contents
+ * Viewport Width: 640
+ */
+
+declare(strict_types=1);
+
+# Prevent direct access.
+defined('ABSPATH') || exit;
+
+?>
+
+<!-- wp:details -->
+<details class="wp-block-details">
+	<summary><?= esc_html_e('Table of Contents', 'x3p0-ideas') ?></summary>
+	<!-- wp:table-of-contents {"className":"is-style-pull"} /-->
+</details>
+<!-- /wp:details -->
